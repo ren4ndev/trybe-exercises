15744d5cb454196948b13cd7a4d187d99580a775
Renan OLiveira <ren4n.dev@gmail.com>
2020-08-27 19:16:15 -0300

    Exercicio 7 - Refatoração

--- int_des_web/bloco_4/dia_1/nota.js
+++ int_des_web/bloco_4/dia_1/nota.js
@@ -1,17 +1,18 @@
 let porcentagem = 90;
 
-if (porcentagem > 0 && porcentagem < 50) {
-    console.log("F");
-} else if (porcentagem >= 50 && porcentagem < 60) {
-    console.log("E");
-} else if (porcentagem >= 60 && porcentagem < 70) {
-    console.log("D");
-} else if (porcentagem >= 70 && porcentagem < 80) {
-    console.log("C");
-} else if (porcentagem >= 80 && porcentagem < 90) {
-    console.log("B");
-} else if (porcentagem >= 90 && porcentagem <=100) {
-    console.log("A");
-} else {
+if (porcentagem < 0 || porcentagem > 100) {
     console.log("Esta nota não pode ser calculada");
+} else if (porcentagem >= 90) {
+    console.log("A");
+} else if (porcentagem >= 80) {
+    console.log("B");
+} else if (porcentagem >= 70) {
+    console.log("C");
+} else if (porcentagem >= 60) {
+    console.log("D");
+} else if (porcentagem >= 50) {
+    console.log("E");
+} else {
+    console.log("F");
 }
+
