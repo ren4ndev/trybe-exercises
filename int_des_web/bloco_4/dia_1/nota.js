ae8d9bb1922dd158c6084de147055d77304bdcc7
Renan OLiveira <ren4n.dev@gmail.com>
2020-08-27 18:44:25 -0300

    Exercicio 7 - Nota (finalizado)

--- int_des_web/bloco_4/dia_1/nota.js
+++ int_des_web/bloco_4/dia_1/nota.js
@@ -1,28 +1,17 @@
-let porcentagem = 1000;
+let porcentagem = 90;
 
-switch (porcentagem) {
-    case (porcentagem < 0 || porcentagem > 100):
-        console.log("Esta nota não pode ser calculada");
-        break;
-    case porcentagem < 50:
-        console.log("F");
-        break;
-    case porcentagem >= 50 && porcentagem < 60:
-        console.log("E");
-        break;
-    case porcentagem >= 60 && porcentagem < 70:
-        console.log("D");
-        break;
-    case porcentagem >= 70 && porcentagem < 80:
-        console.log("C");
-        break;
-    case porcentagem >= 80 && porcentagem < 90:
-        console.log("B");
-        break;
-    case porcentagem > 90 && porcentagem <=100:
-        console.log("A");
-        break;
-    default:
-        console.log("Não entendi");
+if (porcentagem > 0 && porcentagem < 50) {
+    console.log("F");
+} else if (porcentagem >= 50 && porcentagem < 60) {
+    console.log("E");
+} else if (porcentagem >= 60 && porcentagem < 70) {
+    console.log("D");
+} else if (porcentagem >= 70 && porcentagem < 80) {
+    console.log("C");
+} else if (porcentagem >= 80 && porcentagem < 90) {
+    console.log("B");
+} else if (porcentagem >= 90 && porcentagem <=100) {
+    console.log("A");
+} else {
+    console.log("Esta nota não pode ser calculada");
 }
-
