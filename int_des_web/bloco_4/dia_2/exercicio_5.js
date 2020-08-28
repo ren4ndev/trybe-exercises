86f2bf1cff16c7cc21338ad2fea327d72e78eb83
Renan OLiveira <ren4n.dev@gmail.com>
2020-08-28 15:52:18 -0300

    Exercicio 7 - Finalizado

--- int_des_web/bloco_4/dia_2/exercicio_5.js
+++ int_des_web/bloco_4/dia_2/exercicio_5.js
@@ -1,12 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let valor = 0;
 
 for (i = 0; i < numbers.length; i++) {
-    if (soma < numbers [i]) {
-        soma = numbers[i];
+    if (valor < numbers [i]) {
+        valor = numbers[i];
     }
 }
 
-console.log(soma);
+console.log(valor);
 
 
