db7d5ce0b896e7be557b6982492701737da63c63
Renan OLiveira <ren4n.dev@gmail.com>
2020-09-18 18:53:51 -0300

    Iniciando uso da biblioteca Pikaday para trabalhar com datas

--- int_des_web/bloco_6/dia_2/script.js
+++ int_des_web/bloco_6/dia_2/script.js
@@ -3,6 +3,14 @@ submit.addEventListener('click', function (event) {
 	event.preventDefault();
 });
 
+let picker = new Pikaday({ 
+	field: document.querySelector('#datepicker'),
+	onSelect: function(date) {
+		console.log(picker.toString());
+	}
+});
+console.log(picker);
+
 window.onload = function () {
 	montaOptions(estados);
 };
