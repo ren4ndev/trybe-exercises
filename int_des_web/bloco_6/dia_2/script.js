f7baf120bbac2c890b17afb83b48901beb651765
Renan OLiveira <ren4n.dev@gmail.com>
2020-09-18 19:38:36 -0300

    Finalizando os exercícios bloco 6.2

--- int_des_web/bloco_6/dia_2/script.js
+++ int_des_web/bloco_6/dia_2/script.js
@@ -1,15 +1,33 @@
 const submit = document.querySelector('#btn-submit');
 submit.addEventListener('click', function (event) {
 	event.preventDefault();
+	new window.JustValidate('.js-form', {
+		submitHandler: function (form, values, ajax) {
+			ajax({
+				url: 'https://just-validate-api.herokuapp.com/submit',
+				method: 'POST',
+				data: values,
+				async: true,
+				callback: function (response) {
+					alert('AJAX submit successful! \nResponse from server:' + response)
+				},
+				error: function (response) {
+					alert('AJAX submit error! \nResponse from server:' + response)
+				}
+			});
+		}
+	})	
 });
 
 let picker = new Pikaday({ 
 	field: document.querySelector('#datepicker'),
 	onSelect: function(date) {
-		console.log(picker.toString());
+		console.log(date);
 	}
 });
 console.log(picker);
+
+
 
 window.onload = function () {
 	montaOptions(estados);
