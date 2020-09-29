8012a0508ce8014fa35b929bb967eaee160b77fe
Renan OLiveira <ren4n.dev@gmail.com>
2020-09-29 09:18:31 -0300

    Terminando exercicios do bloco 7.2

--- int_des_web/bloco_7/dia_2/exercicio.js
+++ int_des_web/bloco_7/dia_2/exercicio.js
@@ -37,3 +37,29 @@ console.log(objLength(lesson1));
 //	Exercicio 4
 const listValues = obj => Object.values(obj);
 console.log(listValues(lesson1));
+
+//  Exercício 5
+const allLessons = {
+  lesson1: Object.assign({}, lesson1),
+  lesson2: Object.assign({}, lesson2),
+  lesson3: Object.assign({}, lesson3)
+};
+console.log(allLessons);
+
+//  Exercício 6
+const numberStudents = (allLessons) => {
+  let count = 0
+  for (index in allLessons) {
+    count += allLessons[index].numeroEstudantes;
+  }
+  return count;
+}
+console.log(numberStudents(allLessons));
+
+//  Exercicio 7
+const getValueByNumber = (obj, pos) => Object.values(obj)[pos];
+console.log(getValueByNumber(lesson3, 0));
+
+//  Exercicio 8
+const verifyPair = (obj, key, value) => obj[key] === value ? true : false;
+console.log(verifyPair(lesson3, 'materia', 'Matemática'))
