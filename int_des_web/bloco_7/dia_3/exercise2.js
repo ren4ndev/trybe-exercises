759790481b12a8923b5117513ecc63ff45fb6d84
Renan OLiveira <ren4n.dev@gmail.com>
2020-09-30 10:45:56 -0300

    Começando exercícios bloco 7.3

---
+++ int_des_web/bloco_7/dia_3/exercise2.js
@@ -0,0 +1,28 @@
+//  Exercicio 2
+
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+//  implemente seus testes aqui
+//  Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+
+//  Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+//  Verifique se o array passado por parâmetro não sofreu alterações
+let arr = [1, 2, 3, 4];
+myRemove(arr, 3);
+assert.deepStrictEqual(arr, [1, 2, 3, 4]);
+
+//  Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
