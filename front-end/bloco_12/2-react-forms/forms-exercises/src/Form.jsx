5f7ea06e50f761222d21608c840a33cc6e53a759
Renan Oliveira <oliveira.renan.rg@gmail.com>
2020-11-11 15:31:10 -0300

    Controlando elementos do Forms

--- front-end/bloco_12/2-react-forms/forms-exercises/src/Form.jsx
+++ front-end/bloco_12/2-react-forms/forms-exercises/src/Form.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 
 class Form extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDescription = this.handleDescription.bind(this);
+
+    this.state = {
+      estado: 'RJ',
+      nome: '',
+      sobrenome: '',
+      descricao: ''
+    }
+  }
+
+  handleDescription(event) {
+    this.setState({ descricao: event.target.value });
+  }
+
+  handleChange(event) {
+    this.setState({ estado: event.target.value });
+  }
   render() {
     return (
-      <div></div>
+      <form className="form">
+        <label htmlFor="">
+          Selecione seu estado:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="RJ">Rio de Janeiro</option>
+            <option value="RS">Rio Grande do Sul</option>
+            <option value="RN">Rio Grande do Norte</option>
+          </select>
+        </label>
+        <label htmlFor="">
+          Nome:
+          <input type="text" value={this.state.nome} onChange />
+        </label>
+        <label htmlFor="">
+          Sobrenome:
+          <input type="text"/>
+        </label>
+        <label htmlFor="">
+          Descrição:
+          <textarea value={this.state.descricao} onChange={this.handleDescription} />
+        </label>
+      </form>
     );
   }
 }
