de423820e12b5a0c66c347c3255010c4eb4c101c
Renan Oliveira <oliveira.renan.rg@gmail.com>
2020-11-11 16:21:14 -0300

    Terminando pre-aula

--- front-end/bloco_12/2-react-forms/forms-exercises/src/Form.jsx
+++ front-end/bloco_12/2-react-forms/forms-exercises/src/Form.jsx
@@ -5,17 +5,20 @@ class Form extends React.Component {
     super();
 
     this.handleChange = this.handleChange.bind(this);
+    this.fileInput = React.createRef();
 
     this.state = {
       estado: 'RJ',
       nome: '',
       sobrenome: '',
-      descricao: ''
+      descricao: '',
+      isPcd: false
     }
   }
 
   handleChange({ target }) {
-    const { name, value } = target;
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value
     });
@@ -24,26 +27,49 @@ class Form extends React.Component {
   render() {
     return (
       <form className="form">
-        <label htmlFor="">
-          Selecione seu estado:
-          <select name="estado" value={this.state.value} onChange={this.handleChange}>
-            <option value="RJ">Rio de Janeiro</option>
-            <option value="RS">Rio Grande do Sul</option>
-            <option value="RN">Rio Grande do Norte</option>
-          </select>
-        </label>
-        <label htmlFor="">
-          Nome:
-          <input type="text" name="nome" value={this.state.nome} onChange={this.handleChange} />
-        </label>
-        <label htmlFor="">
-          Sobrenome:
-          <input type="text" name="sobrenome" value={this.state.sobrenome} onChange={this.handleChange} />
-        </label>
-        <label htmlFor="">
-          Descrição:
-          <textarea name="descricao" value={this.state.descricao} onChange={this.handleChange} />
-        </label>
+        <fieldset>
+
+          <label htmlFor="">
+            Selecione seu estado:
+            <select name="estado" value={this.state.value} onChange={this.handleChange}>
+              <option value="RJ">Rio de Janeiro</option>
+              <option value="RS">Rio Grande do Sul</option>
+              <option value="RN">Rio Grande do Norte</option>
+            </select>
+          </label>
+
+          <label htmlFor="">
+            Nome:
+            <input type="text" name="nome" value={this.state.nome} onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="">
+            Sobrenome:
+            <input type="text" name="sobrenome" value={this.state.sobrenome} onChange={this.handleChange} />
+          </label>
+          
+        </fieldset>
+        
+
+        <fieldset>
+
+          <label htmlFor="">
+            Descrição:
+            <textarea name="descricao" value={this.state.descricao} onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="">
+            PCD:
+            <input type="checkbox" name="isPcd" checked={this.state.isPcd} onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="">
+            Anexe seu currículo
+            <input type="file" ref={this.fileInput} />
+          </label>
+
+        </fieldset>
+        
       </form>
     );
   }
